70f32a54c94783cdb6a9140da0e2b9eb8626b660
Isaiah Booher <isaiahbooher2@gmail.com>
2023-04-21 11:41:43 -0500

    Added Financial Report

--- Backend/database.sql
+++ Backend/database.sql
@@ -1,61 +1,90 @@
-create table tblFarms (
-	Farm_ID INT,
+CREATE TABLE tblFarms (
+    Farm_ID INT NOT NULL AUTO_INCREMENT,
     Farm_Name varchar(25),
     Street_Address_1 varchar(30),
     Street_Address_2 varchar(30),
     City varchar(25),
     State varchar(15),
     Zip INT,
-    Primary Key (Farm_ID)
+    PRIMARY KEY (Farm_ID)
 );
-create table tblProducts (
-	Product_ID INT,
-    Farm_Name varchar(25),
+
+CREATE TABLE tblProducts (
+    Product_ID INT NOT NULL AUTO_INCREMENT,
+    Farm_ID INT,
     Prod_Type varchar(15),
     Fixed_Cost decimal(9,2),
     Date_Planted DATETIME,
     Date_Harvested DATETIME,
     Harvest_Weight_LBS decimal(9,2),
     Quantity INT,
-    Primary Key (Product_ID)
+    PRIMARY KEY (Product_ID),
+    FOREIGN KEY (Farm_ID) REFERENCES tblFarms(Farm_ID)
 );
-create table tblUsers (
-	User_ID INT,
+
+CREATE TABLE tblUsers (
+    User_ID INT NOT NULL AUTO_INCREMENT,
     Farm_ID INT,
     First_Name varchar(25),
     Last_Name varchar(25),
     Email varchar(30),
     UserPassword varchar(30),
     Phone varchar(10),
-    Primary Key (User_ID)
+    PRIMARY KEY (User_ID),
+    FOREIGN KEY (Farm_ID) REFERENCES tblFarms(Farm_ID)
 );
-create table tblEmployees (
-	Emp_ID INT,
-    Farm_Name varchar(20),
+
+CREATE TABLE tblEmployees (
+    Emp_ID INT NOT NULL AUTO_INCREMENT,
+    Farm_ID INT,
     EmpFirstName varchar(25),
     EmpLastName varchar(25),
     Pay_Rate_hr decimal(3,2),
     Position varchar(15),
-    Primary Key (Emp_ID)
+    PRIMARY KEY (Emp_ID),
+    FOREIGN KEY (Farm_ID) REFERENCES tblFarms(Farm_ID)
 );
 
-create table tblTasks (
-	Task_ID INT,
+CREATE TABLE tblTasks (
+    Task_ID INT NOT NULL AUTO_INCREMENT,
+    Prod_ID INT,
     Title varchar(15),
-    Prod_Type varchar(15),
     Description varchar(250),
     Total_Time_Min INT,
-    Emp_FirstName varchar(15),
-    Emp_LastName varchar(15),
-    Primary Key (Task_ID)
+    Emp_ID INT,
+    PRIMARY KEY (Task_ID),
+    FOREIGN KEY (Prod_ID) REFERENCES tblProducts(Product_ID),
+    FOREIGN KEY (Emp_ID) REFERENCES tblEmployees(Emp_ID)
 );
 
-create table tblInventory (
-	Inventory_ID int,
-    Farm_ID int,
+CREATE TABLE tblInventory (
+    Inventory_ID INT NOT NULL AUTO_INCREMENT,
+    Farm_ID INT,
     Item varchar(15),
     Item_Description varchar(250),
     Item_Cost decimal(9,2),
     Date_Recorded DATETIME,
-    Primary Key (Inventory_ID)
+    PRIMARY KEY (Inventory_ID),
+    FOREIGN KEY (Farm_ID) REFERENCES tblFarms(Farm_ID)
 );
+
+CREATE TABLE tblSessions (
+    Session_ID INT NOT NULL AUTO_INCREMENT,
+    User_ID INT,
+    Session_Start DATETIME,
+    Session_End DATETIME,
+    PRIMARY KEY (Session_ID),
+    FOREIGN KEY (User_ID) REFERENCES tblUsers(User_ID)
+);
+
+CREATE TABLE tblCosts (
+    Cost_ID INT NOT NULL AUTO_INCREMENT,
+    Farm_ID INT,
+    Cost_Date DATETIME,
+    Cost_Type varchar(25),
+    Cost_Description varchar(250),
+    Cost_Amount decimal(9,2),
+    PRIMARY KEY (Cost_ID),
+    FOREIGN KEY (Farm_ID) REFERENCES tblFarms(Farm_ID)
+);
+
